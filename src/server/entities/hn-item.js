aabadb2f0f28f4837e79cc1175734be71ae07123
Christopher Darroch <christopher.darroch@gmail.com>
2018-01-06 18:27:17 +0100

    feat: cache retrieved item details for one minute.
    This strategy will need revision, since story item types will accrue votes, comments, etc. over a potentially shorter time-frame.

--- src/server/entities/hn-item.js
+++ src/server/entities/hn-item.js
@@ -1,4 +1,7 @@
 const fetch = require('node-fetch');
+const Cache = require('./cache');
+
+const itemCache = new Cache({ expire: 60 });
 
 class HnItem {
     constructor(id) {
@@ -6,8 +9,18 @@ class HnItem {
     }
 
     fetch() {
+        const id = this._id;
+        // Check the cache first before heading off to the interwebs.
+        const cachedResults = itemCache.get(id);
+        if (!!cachedResults) {
+            return Promise.resolve(cachedResults);
+        }
         return fetch(`https://hacker-news.firebaseio.com/v0/item/${this._id}.json`)
             .then(res => res.json())
+            .then(data => {
+                itemCache.set(id, data);
+                return data;
+            })
             .catch(err => {
                 throw new Error(err);
             })
