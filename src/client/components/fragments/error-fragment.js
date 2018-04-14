67ad5c3c1fb9d5790ed643c4a38d1472aa14096d
Christopher Darroch <cdarroch@atlassian.com>
2018-04-14 10:49:58 +1000

    fix: actually output the error when it occurs. whoops.

--- src/client/components/fragments/error-fragment.js
+++ src/client/components/fragments/error-fragment.js
@@ -4,5 +4,5 @@ export default (err) => html`
     <p>😭 Hmm, something went wrong.</p>
     <p>You may be offline... I just haven't handled the exact problem yet to tell you what happened.</p>
     <p>In the meantime, you'll get the cryptic errors below.</p>
-    <pre><code>${JSON.stringify(err)}</code></pre>
+    <pre><code>${err}</code></pre>
 `;
