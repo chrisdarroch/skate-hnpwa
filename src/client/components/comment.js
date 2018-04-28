6f23cbfe979249578ef76aa0a0b56d6cdf765403
Christopher Darroch <christopher.darroch@gmail.com>
2018-04-28 13:10:13 +0200

    feat: if a comment fails to load, it will provide a retry button to attempt fetching the data again.

--- src/client/components/comment.js
+++ src/client/components/comment.js
@@ -11,8 +11,28 @@ function getArticle(id) {
     // todo: set or inject the base URL during compilation.
     const url = `/api/item/${id}`;
     return fetch(url)
-        .then(resp => resp.json())
-        .then(data => data);
+        .then(resp => {
+            if (!resp.status || resp.status >= 400) {
+                throw resp.json();
+            }
+            return resp.json();
+        });
+}
+
+function commentFragment(article) {
+    let commentBody = document.createElement('div');
+    commentBody.className = 'hncomment__content';
+    commentBody.innerHTML = article.text;
+    return html`
+        <div class="hncomment">
+            <ul class="hnitem__metadata">
+                <li class="metadata__author">by <span>${article.by}</span></li>
+                <li class="metadata__time">${timeFragment(article.time)}</li>
+            </ul>
+            ${commentBody}
+            ${renderReplies(article)}
+        </div>
+    `;
 }
 
 function renderReplies(article) {
@@ -24,10 +44,31 @@ function renderReplies(article) {
     return '';
 }
 
+function retryCommentFragment(err) {
+    return html`
+        <div class="hncomment hncomment--error">
+            <p class="error">😭 We couldn't load that comment.</p>
+            <button class="hncomment__action hncomment__action--retry" type="button">Retry</button>
+        </div>
+    `;
+}
+
 export default class Comment extends BaseComponent {
     static is = 'hnpwa-comment'
     static get props() {
         return { id: props.id };
+    }
+    connected() {
+        this.__retryHandler = e => {
+            let el = e.target;
+            if (el.classList.contains('hncomment__action--retry')) {
+                this.updated(); // trigger a new render
+            }
+        };
+        this.addEventListener('click', this.__retryHandler);
+    }
+    disconnected() {
+        this.removeEventListener('click', this.__retryHandler);
     }
     render({ props, state }) {
         if (!this.props.id) {
@@ -35,21 +76,10 @@ export default class Comment extends BaseComponent {
         }
         return html`
         ${until(
-            getArticle(this.props.id).then(article => {
-                let commentBody = document.createElement('div');
-                commentBody.className = 'hncomment__content';
-                commentBody.innerHTML = article.text;
-                return html`
-                    <div class="hncomment">
-                        <ul class="hnitem__metadata">
-                            <li class="metadata__author">by <span>${article.by}</span></li>
-                            <li class="metadata__time">${timeFragment(article.time)}</li>
-                        </ul>
-                        ${commentBody}
-                        ${renderReplies(article)}
-                    </div>
-                `;
-            }),
+            getArticle(this.props.id)
+                .then(commentFragment)
+                .catch(retryCommentFragment)
+            ,
             html`
                 <p class="loading">🤔 let's see what people said in response...</p>
             `
