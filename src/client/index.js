a7b737b04ba2f2dadcdd2ae86d3681f87393b36a
Christopher Darroch <cdarroch@atlassian.com>
2018-06-02 22:47:37 +0200

    feat: use a custom elements v1 polyfill that works in IE 11. Remove shadow DOM polyfill.
    We're not using the shadow DOM right now, so no need to polyfill it.

--- src/client/index.js
+++ src/client/index.js
@@ -1,21 +1,4 @@
-import '!file-loader?name=ce-es5-shim.js!./fills/ce-es5-shim.js';
-import '!file-loader?name=ce-sd-fill.js!./fills/ce-sd-fill.js';
+import 'document-register-element';
 import './app/index.css';
 
-function mount() {
-    import('./app/index.js');
-}
-
-function script(src, done) {
-    var scr = document.createElement('script');
-    scr.async = false;
-    scr.onload = done;
-    scr.src = src;
-    document.head.appendChild(scr);
-}
-
-if (window.customElements) {
-    script('/ce-es5-shim.js', mount);
-} else {
-    script('/ce-sd-fill.js', mount);
-}
+import('./app/index.js');
