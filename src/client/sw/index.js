855effc6a10bdc8326de5c18ac1a107e803529e7
Christopher Darroch <cdarroch@atlassian.com>
2018-06-03 21:39:49 +0200

    feat: unpack and seed cache with items from requests to list endpoints.
    In a naive REST implementation, the list endpoint would return the IDs of items to make subsequent requests for.
    Our REST endpoint for lists returns a collection of items, which makes it not really RESTful.
    This may indicate that the REST design needs some re-thinking.
    
    An alternative design might see us do away with the REST requests altogether and adopt websockets, so we could keep a persistent connection to the server and reduce the cost of making N+1 requests for items.
    
    Meanwhile, in the current reality where REST is our transfer mechanism and we care about providing data to users quickly, unpacking data from a given response to seed a separate cache seems like a reasonable compromise.

--- src/client/sw/index.js
+++ src/client/sw/index.js
@@ -1,6 +1,10 @@
 console.log(`hi, is it me you're looking for?`);
 
 const baseUrl = self.location.origin;
+const ourCacheNames = {
+    ITEMS: 'skate-hnpwa-items',
+    LISTS: 'skate-hnpwa-list'
+}
 
 workbox.core.setCacheNameDetails({prefix: 'skate-hnpwa'});
 
@@ -10,10 +14,63 @@ workbox.clientsClaim();
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
 workbox.routing.registerRoute(
-    new RegExp(`^${baseUrl}/api/`),
+    new RegExp(`^${baseUrl}/api/item/(\\d+)`),
+    workbox.strategies.cacheFirst({
+        cacheName: ourCacheNames.ITEMS,
+    }),
+    'GET'
+);
+
+workbox.routing.registerRoute(
+    new RegExp(`^${baseUrl}/api/(?!item)`),
     workbox.strategies.staleWhileRevalidate({
-        cacheName: "skate-hnpwa-data",
-        plugins: []
+        cacheName: ourCacheNames.LISTS,
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [200]
+            }),
+            {
+            // Take the collection of items returned in the list response, then individually
+            // cache each item in the collection.
+            // todo: set appropriate cache + date headers in the request and response objects.
+            // note: I'm using cacheWillUpdate instead of cacheDidUpdate because of a bug in workbox.
+            // see: https://github.com/GoogleChrome/workbox/issues/1515
+            cacheWillUpdate: babelHelpers.asyncToGenerator(
+                function* ({ request, response }) {
+                    const cachedResponse = response && response.clone();
+                    if (!cachedResponse) {
+                        return;
+                    }
+
+                    const newResponse = cachedResponse.clone()
+                    const data = yield cachedResponse.json();
+                    if (data && data.items) {
+                        const cache = yield caches.open(ourCacheNames.ITEMS);
+                        for (let i = 0, ii = data.items.length; i < ii; i++) {
+                            let item = data.items[i];
+                            let body = JSON.stringify(item);
+                            let { headers, referrerPolicy, vary } = request;
+                            let req = new Request(`${baseUrl}/api/item/${item.id}`, {
+                                headers,
+                                referrer: `/item/${item.id}`,
+                                referrerPolicy,
+                                vary,
+                            });
+                            let res = new Response(body, {
+                                headers: new Headers({
+                                    'Content-Type': 'application/json',
+                                    'Content-Length': body.length,
+                                    'Date': new Date().toUTCString(),
+                                })
+                            });
+                            cache.put(req, res);
+                        }
+                    }
+
+                    return newResponse;
+                }
+            ),
+        }]
     }),
     'GET'
 );
