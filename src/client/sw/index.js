4350d4a746fb8a1e6257d2bbdf3abd262c2b2e98
Christopher Darroch <cdarroch@atlassian.com>
2018-06-03 19:43:10 +0200

    fix: the service worker now responds to application routes.
    In the previous arrangement, the API routes were not being cached whatsoever, because the regex was matching against the full href of the URL rather than its path.
    
    There's an odd incongruity, though: if you register regexes for the whitelist or blacklist of a navigation route, these match against the path + search of the URL, not the full href.
    
    This seems like something that could be improved in workbox.

--- src/client/sw/index.js
+++ src/client/sw/index.js
@@ -1,5 +1,7 @@
 console.log(`hi, is it me you're looking for?`);
 
+const baseUrl = self.location.origin;
+
 workbox.core.setCacheNameDetails({prefix: 'skate-hnpwa'});
 
 workbox.skipWaiting();
@@ -8,7 +10,7 @@ workbox.clientsClaim();
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
 workbox.routing.registerRoute(
-    new RegExp(`^/api/`),
+    new RegExp(`^${baseUrl}/api/`),
     workbox.strategies.staleWhileRevalidate({
         cacheName: "skate-hnpwa-data",
         plugins: []
@@ -19,10 +21,14 @@ workbox.routing.registerRoute(
 // todo: pull from routes/index.js ...
 // the problem is this file isn't currently compiled via webpack, and
 // the google workbox plugin doesn't allow setting an entrypoint as the sw entry. Weird.
+// note: the route whitelist and blacklist are matched on pathname + search,
+//       whereas normal routes are matched on the full URL.
+//       This incongruity seems odd.
+// see: https://github.com/GoogleChrome/workbox/issues/1514
 const appRoutes = [
-    new RegExp('^/'),
-    new RegExp('^/(top|new|best|show|ask|job)/?'),
-    new RegExp('^/item/([^/]*)/?'),
+    new RegExp(`^/?$`),
+    new RegExp(`^/(top|new|best|show|ask|job)/?$`),
+    new RegExp(`^/item/(\\d+)/?$`),
 ];
 workbox.routing.registerNavigationRoute('/index.html', {
     whitelist: appRoutes
