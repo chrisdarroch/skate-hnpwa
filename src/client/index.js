38f97d4f1fc1d3f5ba4f3d770023924417c83034
Christopher Darroch <christopher.darroch@gmail.com>
2018-06-19 23:45:51 +0200

    feat: avoid analytics blocking render.

--- src/client/index.js
+++ src/client/index.js
@@ -3,13 +3,14 @@ import '!file-loader?name=ce-sd-fill.js!./fills/ce-sd-fill.js';
 import './app/index.css';
 
 function mount() {
-    import('./app/index.js');
+    import('./app/index.js')
+        .then(() => script('https://www.googletagmanager.com/gtag/js?id=UA-121101247-1'))
 }
 
 function script(src, done) {
     var scr = document.createElement('script');
     scr.async = false;
-    scr.onload = done;
+    scr.onload = typeof done === 'function' ? done : undefined;
     scr.src = src;
     document.head.appendChild(scr);
 }
